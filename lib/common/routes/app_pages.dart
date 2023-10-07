48256364aad24eb0bddcdcb38434ee1166634922
Fadza <fadza0077@gmail.com>
2023-10-07 08:52:08 +0700

    change route

--- lib/common/routes/app_pages.dart
+++ lib/common/routes/app_pages.dart
@@ -5,9 +5,6 @@ import 'package:web_pancasila_aceh/app/pages/history_page/history_page_binding.d
 import 'package:web_pancasila_aceh/app/pages/history_page/history_page_view.dart';
 import 'package:web_pancasila_aceh/app/pages/landing_page/landing_page_binding.dart';
 import 'package:web_pancasila_aceh/app/pages/landing_page/landing_page_view.dart';
-import 'package:web_pancasila_aceh/app/pages/recognize_page/recognize_page_binding.dart';
-import 'package:web_pancasila_aceh/app/pages/recognize_page/recognize_page_view.dart';
-
 
 
 part 'app_routes.dart';
@@ -15,7 +12,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LANDING_PAGE;
+  static const INITIAL = Routes.HISTORY_PAGE;
 
   static final routes = [
     GetPage(
@@ -29,14 +26,14 @@ class AppPages {
       binding: HistoryPageBinding(),
     ),
     GetPage(
+      name: _Paths.LANDING_PAGE,
+      page: () => LandingPageView(),
+      binding: LandingPageBinding(),
+    ),
+    GetPage(
       name: _Paths.HISTORY_PAGE,
       page: () => EconomicPageView(),
       binding: EconomicPageBinding(),
-    ),
-    GetPage(
-      name: _Paths.RECOGNIZE_PAGE,
-      page: () => RecognizePageView(),
-      binding: RecognizePageBinding(),
     ),
   ];
 }
