396429a1769e39562662c28306f3b3d29117504e
SyahranFD <fadhilrafa1@gmail.com>
2023-10-12 10:15:28 +0700

    feat : [SF] add history component two desktop

--- lib/app/pages/history_page/history_page_view.dart
+++ lib/app/pages/history_page/history_page_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:web_pancasila_aceh/app/global_component/hero_component.dart';
+import 'package:web_pancasila_aceh/app/pages/history_page/components/history_component_two_desktop.dart';
 import 'package:web_pancasila_aceh/common/base/abstract_responsive_state.dart';
 
 
@@ -14,13 +16,15 @@ class HistoryPageView extends ResponsiveScreenState {
   Widget buildDesktopPage() {
     // TODO: implement buildDesktopPage
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.white,
       body: Container(
         width: double.infinity,
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Text("This is Desktop Page")
+              HeroComponent(imagePath: "assets/images/sejarah_landing.jpg", headingText: "Sejarah Provinsi Aceh", subHeadingText: "Aceh dari Zaman ke Zaman"),
+              SizedBox(height: 50),
+              HistoryComponentTwoDesktop(),
             ],
           ),
         ),
