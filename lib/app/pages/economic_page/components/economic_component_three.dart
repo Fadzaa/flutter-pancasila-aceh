0a6dd1d03cdc1dce91b00e6efeebaf2cea2c173c
VeinVoid <vickokudus@gmail.com>
2023-10-20 21:31:37 +0700

    feat : [VA] add economic page desktop

---
+++ lib/app/pages/economic_page/components/economic_component_three.dart
@@ -0,0 +1,60 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:web_pancasila_aceh/app/pages/economic_page/widget/dekstop/economic_card1_widget.dart';
+import 'package:web_pancasila_aceh/app/pages/economic_page/widget/dekstop/economic_card2_widget.dart';
+import 'package:web_pancasila_aceh/common/helper/themes.dart';
+
+class EconomicalComponentThree extends StatelessWidget {
+  const EconomicalComponentThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: Column(
+        children: [
+          Center(
+            child: Text(
+              "Mau tau tantangannya?",
+              style: tsHeading3MediumBlack,
+            ),
+          ),
+          Center(
+            child: Text(
+              "Tantangan Ekonomi di Aceh",
+              style: tsHeading1BoldBlack,
+            ),
+          ),
+          SizedBox(height: 100),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                EconomicCardVertical(
+                  professionImage: "assets/images/perekonomian_pariwisata.png", 
+                  professionName: "Infrastruktur", 
+                  professionDescription: "Keterbatasan infrastruktur dan konektivitas dapat menghambat pertumbuhan ekonomi dan akses ke pasar."
+                ),
+                SizedBox(width: 50),
+                EconomicCardVertical(
+                  professionImage: "assets/images/perekonomian_pariwisata.png", 
+                  professionName: "Infrastruktur", 
+                  professionDescription: "Keterbatasan infrastruktur dan konektivitas dapat menghambat pertumbuhan ekonomi dan akses ke pasar."
+                )
+              ],
+            ),
+          ),
+
+          SizedBox(height: 30),
+          EconomicCardHorizontal(
+            professionImage: "assets/images/perekonomian_pariwisata.png", 
+            professionName: "Infrastruktur", 
+            professionDescription: "Keterbatasan infrastruktur dan konektivitas dapat menghambat pertumbuhan ekonomi dan akses ke pasar."
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/pages/economic_page/widget/dekstop/economic_card1_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:web_pancasila_aceh/common/helper/themes.dart';
+
+class EconomicCardHorizontal extends StatelessWidget {
+  const EconomicCardHorizontal({
+    super.key,
+    required this.professionImage,
+    required this.professionName,
+    required this.professionDescription
+    });
+
+    final String professionImage;
+    final String professionName;
+    final String professionDescription;
+
+  @override
+  Widget build(BuildContext context) {
+
+    double width = MediaQuery.sizeOf(context).width;
+    double height = MediaQuery.sizeOf(context).height;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: width * 0.15),
+      width: width,
+      height: height * 0.88,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 255, 255, 255),
+        borderRadius: BorderRadius.all(
+            Radius.circular(28)
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            offset: Offset(0, 3),
+            blurRadius: 6,
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: width,
+            height: height * 0.55,
+            margin: EdgeInsets.only(top: 32, left: 43, right: 43),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(28),
+              image: DecorationImage(
+                image: AssetImage(professionImage),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 28),
+          Text(
+            professionName,
+            style: tsHeading2SemiBoldBlack,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 28),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 94),
+            child: Text(
+              professionDescription,
+              style: tsHeading4MediumBlack,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
