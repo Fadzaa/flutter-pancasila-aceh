3fa6cef74839fcf7f1e5cc4ca58ceb51dd72d576
Fadza <fadza0077@gmail.com>
2023-10-21 23:41:41 +0700

    feat: [FA] add mobile landing component four

--- lib/app/pages/landing_page/components/landing_component_four.dart
+++ lib/app/pages/landing_page/components/landing_component_four.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:web_pancasila_aceh/common/base/abstract_responsive_state.dart';
 import 'package:web_pancasila_aceh/common/helper/themes.dart';
 
-class LandingComponentFour extends StatelessWidget {
-  const LandingComponentFour({super.key});
+class LandingComponentFour extends ResponsiveScreenState {
+  LandingComponentFour({
+    Key? key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    return super.build(context);
+  }
+
+  @override
+  Widget buildDesktopPage(BuildContext context) {
+    // TODO: implement buildDesktopPage
     return Container(
       margin: EdgeInsets.only(left: 88, top: 34, right: 88),
       child: Row(
@@ -104,11 +113,125 @@ class LandingComponentFour extends StatelessWidget {
                   ),
                 ),
               ),
-
             ],
           )
         ],
       ),
     );
+  }
+
+  @override
+  Widget buildMobilePage(BuildContext context) {
+    // TODO: implement buildMobilePage
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Pengen liat pemerintahnya?",
+                          style: tsParagraph12MediumBlack,
+                        ),
+                        Text(
+                          "Galeri Pemimpin Aceh",
+                          style: tsHeadingMobile,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Stack(
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Container(
+                                width: 151,
+                                height: 107,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            "assets/images/gubernur_foto_2.png"),
+                                        fit: BoxFit.fill)),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                width: 161,
+                                height: 135,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(15),
+                                        bottomLeft: Radius.circular(15)),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            "assets/images/gubernur_foto_4.jpg"),
+                                        fit: BoxFit.fill)),
+                              ),
+                            ],
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Container(
+                              width: 160,
+                              height: 110,
+                              margin: EdgeInsets.only(top: 10, right: 35),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          "assets/images/gubernur_foto_3.jpg"),
+                                      fit: BoxFit.fill)),
+                            ),
+                          )
+                        ],
+                      ),
+                      Image.asset("assets/images/gubernur_foto_1.png",
+                          width: 220)
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 35),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Mayor Jenderal TNI (Purn.) Achmad Marzuki",
+                          style: tsParagraph16BoldBlack,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
+                          style: tsParagraph12MediumBlack,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
