6c14480a570bc1d491e849cebc43841e3b1d1dbd
Fadza <fadza0077@gmail.com>
2023-11-08 00:51:02 +0700

    data: [FA] add detail culture mock data

--- lib/common/mock_data/mock_culture_data.dart
+++ lib/common/mock_data/mock_culture_data.dart
@@ -3,32 +3,47 @@ List<Map<String, dynamic>> mockCultureData = [
   {
     'id': 1,
     'name': 'Tari Saman',
-    'description': 'Tari Saman adalah tarian tradisional Aceh yang melibatkan gerakan tangan yang cepat dan ritmik serta vokal yang khas, sering kali diiringi oleh musik gendang.',
-    'image_url': 'https://cdnwpedutorenews.gramedia.net/wp-content/uploads/2022/01/10150442/popmama.jpg'
+    'card_description': 'Tari Saman adalah tarian tradisional Aceh yang melibatkan gerakan tangan yang cepat dan ritmik serta vokal yang khas, sering kali diiringi oleh musik gendang.',
+    'detail_description': 'Tari Saman, juga dikenal sebagai "Saman Dance," adalah tarian tradisional khas Aceh yang memukau dengan gerakan tubuh, tangan, dan suara yang harmonis. Penari duduk dalam barisan, di mana seorang pemimpin atau "imam" memimpin gerakan, yang dimulai dengan gerakan tangan yang lambat namun semakin kompleks seiring berjalannya waktu. Tarian ini menggabungkan kecepatan tangan yang luar biasa dengan nyanyian yang menceritakan cerita atau pesan moral, dan seringkali memiliki pesan kebersamaan dan persatuan. Tari Saman adalah warisan budaya dan seni yang sangat berharga di Aceh dan dikenal di seluruh Indonesia serta di tingkat internasional.',
+    'image_card': 'https://cdnwpedutorenews.gramedia.net/wp-content/uploads/2022/01/10150442/popmama.jpg',
+    'image_detail_1': 'https://asset-2.tstatic.net/aceh/foto/bank/images/tari-saman-atas-dan-tari-ratoh-jaroe-bawah.jpg',
+    'image_detail_2': 'https://indonesiakaya.com/wp-content/uploads/2020/10/tari_saman_ie_1200.jpg',
   },
   {
     'id': 2,
     'name': 'Gamelan Aceh',
-    'description': 'Alat musik tradisional yang digunakan dalam musik dan upacara adat Aceh, biasanya terdiri dari berbagai jenis gong dan alat musik perkusi lainnya.',
-    'image_url' : 'https://pinhome-blog-assets-public.s3.ap-southeast-1.amazonaws.com/2022/08/alat-musik-dari-aceh-4.jpg'
+    'card_description': 'Alat musik tradisional yang digunakan dalam musik dan upacara adat Aceh, biasanya terdiri dari berbagai jenis gong dan alat musik perkusi lainnya.',
+    'detail_description': 'Gamelan Aceh adalah ensambel musik tradisional yang khas dari Provinsi Aceh, Indonesia. Gamelan ini terdiri dari berbagai jenis instrumen musik perkusi seperti gong, kenong, saron, rebab, dan lainnya. Yang membuat Gamelan Aceh unik adalah penggunaan gong yang besar dan kuat yang dikenal sebagai "gong besar Aceh" atau "gong Agung." Selain instrumen-instrumen tersebut, Gamelan Aceh juga sering diiringi oleh vokal atau nyanyian. Musik Gamelan Aceh memiliki melodi yang kaya dan ritmis yang menggambarkan budaya dan sejarah Aceh. Gamelan Aceh sering dimainkan dalam berbagai acara adat, upacara keagamaan, perayaan, dan pertunjukan seni tradisional di Aceh, serta menjadi ekspresi budaya yang penting dan identitas yang kuat bagi masyarakat Aceh.',
+    'image_card' : 'https://pinhome-blog-assets-public.s3.ap-southeast-1.amazonaws.com/2022/08/alat-musik-dari-aceh-4.jpg',
+    'image_detail_1': 'https://berita.99.co/wp-content/uploads/2022/08/gamelan-jawa-tengah.jpg',
+    'image_detail_2': 'https://gpriority.co.id/wp-content/uploads/2021/12/IMG-20211225-WA0030.jpg',
   },
   {
     'id': 3,
     'name': 'Tari Seudati',
-    'description': 'Tarian berkelompok yang menggambarkan kegembiraan dan semangat kebersamaan dengan gerakan kaki yang dinamis dan energik.',
-    'image_url' : 'https://cdn1.katadata.co.id/media/images/thumb/2021/04/06/2021_04_06-09_33_02_d176c026ee91613684e537d81503ee5e_960x640_thumb.jpg'
+    'card_description': 'Tarian berkelompok yang menggambarkan kegembiraan dan semangat kebersamaan dengan gerakan kaki yang dinamis dan energik.',
+    'detail_description': 'Tari Seudati adalah tarian tradisional khas Aceh, Indonesia, yang sangat populer dan memiliki ciri khas gerakan yang dinamis dan energik. Tarian ini biasanya dibawakan oleh sekelompok penari yang berkelompok, biasanya dalam jumlah yang besar, yang menggambarkan semangat kebersamaan dan kegembiraan. Gerakan kaki yang dinamis, irama musik yang kuat, serta penggunaan sapu tangan atau kain putih yang dipegang oleh penari menambahkan nuansa khas tarian ini. Tari Seudati sering dipentaskan dalam berbagai acara adat, perayaan, dan festival seni di Aceh, serta menjadi bagian yang integral dari kebudayaan dan identitas masyarakat Aceh.',
+    'image_card' : 'https://cdn1.katadata.co.id/media/images/thumb/2021/04/06/2021_04_06-09_33_02_d176c026ee91613684e537d81503ee5e_960x640_thumb.jpg',
+    'image_detail_1': 'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1568633398/kb8qxbmwzjs5ktyyx9la.jpg',
+    'image_detail_2': 'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1634025439/05adfb3a00dbba6effeaf33b04bf6a12039406a4e20a3ee43d2baf167fc0b703.jpg',
   },
   {
     'id': 4,
     'name': 'Tari Ratoh Duek',
-    'description': 'Tarian persembahan yang memadukan gerakan lembut dan anggun, seringkali dipentaskan dalam upacara-upacara adat dan pernikahan.',
-    'image_url': 'https://www.dictio.id/uploads/db3342/original/3X/9/f/9f1d6a8738a2aeb6d9f3076258c324f3ccb5c728.jpeg'
+    'card_description': 'Tarian persembahan yang memadukan gerakan lembut dan anggun, seringkali dipentaskan dalam upacara-upacara adat dan pernikahan.',
+    'detail_description': 'Tari Ratoh Duek adalah tarian tradisional Aceh yang indah dan anggun, sering kali dipentaskan dalam berbagai upacara adat, pernikahan, dan perayaan penting. Tarian ini biasanya dibawakan oleh seorang penari wanita tunggal yang memakai pakaian tradisional Aceh yang megah, termasuk "baju kebaya" dan "songket." Gerakan tarian ini menggambarkan keanggunan dan kelembutan, dengan gerakan tangan yang lembut dan elegan. Selain itu, tarian ini juga sering diiringi oleh musik tradisional Aceh yang merdu. Tari Ratoh Duek bukan hanya sebagai tarian persembahan, tetapi juga sebagai simbol kecantikan dan keanggunan budaya Aceh yang mendalam.',
+    'image_card': 'https://www.dictio.id/uploads/db3342/original/3X/9/f/9f1d6a8738a2aeb6d9f3076258c324f3ccb5c728.jpeg',
+    'image_detail_1': 'https://static.wikia.nocookie.net/wikitaria/images/9/96/20160316-tari-ratoh-jaroe_20160316_044116.jpg/revision/latest?cb=20161214225039&path-prefix=id',
+    'image_detail_2': 'https://assets.ayobandung.com/crop/0x0:0x0/750x500/webp/photo/2023/07/07/saman-2971567109.jpg',
   },
   {
     'id': 5,
     'name': 'Gong Aceh',
-    'description': 'Musik tradisional yang menggunakan gong sebagai instrumen utama, sering digunakan dalam upacara adat dan pertunjukan seni.',
-    'image_url' : 'https://www.pinhome.id/blog/wp-content/uploads/2021/08/alat-musik-rapai-aceh.jpg'
+    'card_description': 'Musik tradisional yang menggunakan gong sebagai instrumen utama, sering digunakan dalam upacara adat dan pertunjukan seni.',
+    'detail_description': 'Gong Aceh adalah salah satu jenis alat musik perkusi tradisional yang berasal dari Aceh, Indonesia. Gong ini memiliki peran penting dalam berbagai pertunjukan musik tradisional Aceh, termasuk dalam ansambel gamelan Aceh. Gong-gong Aceh sering digunakan bersama dengan instrumen musik lainnya, seperti kendang, kenong, saron, dan rebab, untuk menciptakan musik yang kaya dan mendalam dengan melodi yang khas. Selain itu, gong-gong Aceh juga digunakan dalam berbagai upacara adat, ritual keagamaan, dan perayaan budaya di Aceh. Gong-gong ini memiliki beragam ukuran dan nada yang berbeda, sehingga menciptakan lapisan suara yang khas dalam musik Aceh, dan mereka juga memiliki nilai simbolis yang dalam dalam budaya Aceh.',
+    'image_url' : 'https://www.pinhome.id/blog/wp-content/uploads/2021/08/alat-musik-rapai-aceh.jpg',
+    'image_detail_1': 'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/02/22/WhatsApp-Image-2023-02-22-at-185617-1450759373.jpeg',
+    'image_detail_2': 'https://gamelancenter.com/wp-content/uploads/2020/04/jual-gong-di-aceh-3.jpg',
   },
 
 ];
