399327a5629adf03b48186e5e84e01a3e7dbe81d
Fadzaa <fadza20@gmail.com>
2024-01-17 08:24:11 +0700

    fix arguments mobile

--- lib/app/pages/detail_page/detail_wisata_page/components/detail_wisata_component_two.dart
+++ lib/app/pages/detail_page/detail_wisata_page/components/detail_wisata_component_two.dart
@@ -52,6 +52,7 @@ class DetailWisataComponentTwo extends ResponsiveScreenState {
     final Size mediaQuery = MediaQuery.of(context).size;
     final double width = mediaQuery.width;
     final double height = mediaQuery.height;
+    Tour tour = Get.arguments;
 
     return Container(
         margin: EdgeInsets.symmetric(horizontal: width * 0.1),
@@ -61,21 +62,21 @@ class DetailWisataComponentTwo extends ResponsiveScreenState {
           children: [
             SizedBox(height: height * 0.05),
             Text('Aceh indah bukan?', style: tsParagraph12MediumGrey),
-            Text('Pantai Pasir Putih', style: tsHeading4BoldGrey),
+            Text(tour.name, style: tsHeading4BoldGrey),
             SizedBox(height: height * 0.04),
             Container(
               height: height * 0.2,
               width: width * 0.9,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(15),
-                child: Image.asset(
-                    'assets/images/wisata_pantai_pasir_putih_description.png',
+                child: Image.network(
+                    tour.cardImage,
                     fit: BoxFit.fitWidth),
               ),
             ),
             SizedBox(height: height * 0.035),
             Text(
-              'Pantai Pasir Putih Aceh, yang terletak di kawasan Lhoknga, Banda Aceh, merupakan salah satu destinasi pantai paling terkenal di Provinsi Aceh, Indonesia. Pantai ini menawarkan pesona alam yang menakjubkan dengan pasir putih yang indah, air laut yang jernih, serta ombak yang cocok untuk berselancar. Wisatawan dapat menikmati berbagai aktivitas, seperti berjemur di pantai, berenang, atau berselancar di ombaknya yang cukup besar. Selain itu, Pantai Pasir Putih juga menjadi saksi keindahan matahari terbenam yang spektakuler. Di sekitar pantai, terdapat warung-warung makan yang menyajikan hidangan khas Aceh, menjadikannya tempat yang ideal untuk bersantai, menikmati keindahan alam, dan mencicipi kuliner lokal yang lezat.',
+              tour.description,
               style: tsParagraph12MediumBlack,
             ),
           ],
